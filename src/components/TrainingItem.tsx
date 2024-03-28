96a3346d10e582bfaff230ba9daa9d8e8136887b
Finnick223 <filipf192@gmail.com>
2024-03-28 10:32:00 +0100

    trainingCards demo

--- src/components/TrainingItem.tsx
+++ src/components/TrainingItem.tsx
@@ -1,20 +1,37 @@
 import { Link } from "react-router-dom";
 import { trainingItemProps } from "./Interfaces";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Paper } from "@mui/material";
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+
 
   export default function TrainingItem(props: trainingItemProps) {
     return (
-      <div className="trainingItem">
-        <Link to={props.id}>
-        <p className="trainingItem--text">{props.trainingName}</p>
-        <p className="trainingItem--text">{props.category}</p>
-        <p className="trainingItem--text">{props.date}</p>
-        </Link>
-        <p>
-          <button className="trainingItem--buttons">edit</button>
-          <button className="trainingItem--buttons">delete</button>
-        </p>
-      </div>
+      <Paper elevation={4} sx={{width: "11vw", m: 2}}>
+        <Card sx={{ width: "11vw" }}>
+          <Link to={props.id} style={{textDecoration: "none", color: "inherit"}}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                {props.date}
+              </Typography>
+              <Typography variant="h5" component="div">
+                {props.trainingName}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {props.category}
+              </Typography>
+            </CardContent>
+          </Link>
+          <CardActions>
+            <Button size="small" variant="contained"><EditNoteOutlinedIcon/></Button>
+            <Button size="small" variant="contained"><DeleteOutlineOutlinedIcon/></Button>
+          </CardActions>
+        </Card>
+      </Paper>
     );
   }
-  
-  
--- src/pages/workout.tsx
+++ src/pages/workout.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import UserCard from '../components/TrainingItem'
+import { Box, Container, CssBaseline } from '@mui/material';
 
 
 function App() {
@@ -16,26 +17,31 @@ function App() {
    }, []);
 
    return (
-    <div className='trainingView'>
-      <UserCard 
-        id="1"
-        trainingName="trening1"
-        category="silowka"
-        date="11.11.2011"
-      />
-      <UserCard 
-        id="2"
-        trainingName="dupa"
-        category="cardio"
-        date="13.11.2011"
-      />
-      <UserCard 
-        id="3"
-        trainingName="trening3"
-        category="hipertrofia"
-        date="15.11.2011"
-      />
-    </div>
+    <> 
+      {/* <Container component="main" maxWidth="xs"> */}
+        <CssBaseline />
+        <Box sx={{display: "inline-flex", textAlign: "center"}}>
+          <UserCard 
+            id="1"
+            trainingName="trening1"
+            category="silowka"
+            date="11.11.2011"
+            />
+          <UserCard 
+            id="2"
+            trainingName="dupa"
+            category="cardio"
+            date="13.11.2011"
+            />
+          <UserCard 
+            id="3"
+            trainingName="trening3"
+            category="hipertrofia"
+            date="15.11.2011"
+            />
+        </Box>
+      {/* </Container> */}
+    </>
   );
 }
 
