14731ee28d2d7d1ac7ce992f2eaa7af1ab1faac9
wojtur <mackowski28@gmail.com>
2024-05-01 11:49:28 +0200

    Missing files

---
+++ src/client/src/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Exercise';
+export * from './Training';
+export * from './TrainingCategory';
+export * from './User';
+export * from './UserMeasurement';
