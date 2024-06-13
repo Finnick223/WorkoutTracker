b0eadb8d5218952ebc5c85183f31034851968f8f
Finnick223 <filipf192@gmail.com>
2024-06-13 21:21:06 +0200

    add materialUI to pagination buttons

--- src/pages/training.page.tsx
+++ src/pages/training.page.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import TrainingCard from '../components/TrainingItem.component.tsx';
-import { Box, CircularProgress, CssBaseline } from '@mui/material';
+import { Box, Button, CircularProgress, CssBaseline, FormControl, InputLabel, MenuItem, Select, Stack } from '@mui/material';
 import { Configuration, Training, TrainingApi } from '../client/src';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
@@ -9,7 +9,7 @@ import useAuthStatus from '../hooks/useAuth.ts';
 function TrainingPage() {
   const [trainings, setTrainings] = useState<Training[]>([]);
   const [page, setPage] = useState<number>(0); // Start from page 0
-  const [size, setSize] = useState<number>(10); // Default page size
+  const [size, setSize] = useState<number>(10);
   const { token } = useAuthStatus();
   const navigate = useNavigate();
 
@@ -45,14 +45,14 @@ function TrainingPage() {
     if (isError) {
       navigate('/error');
     }
-  }, [isSuccess, isError, data, navigate]);
-
-  const handleNextPage = () => {
-    setPage(prevPage => prevPage + 1);
-  };
+  }, [isSuccess, isError, data]);
 
   const handlePreviousPage = () => {
     setPage(prevPage => Math.max(prevPage - 1, 0));
+  };
+  
+  const handleNextPage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
   const handleSizeChange = (event: { target: { value: string; }; }) => {
@@ -73,19 +73,21 @@ function TrainingPage() {
           />
         ))}
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
-        <button onClick={handlePreviousPage} disabled={page === 0}>Previous Page</button>
-        <button onClick={handleNextPage}>Next Page</button>
+      <Stack spacing={2} direction="column" sx={{ display: 'flex', alignItems: 'center', mt: 4 }}>
+      <Box>
+        <Button onClick={handlePreviousPage} disabled={page === 0}>Previous Page</Button>
+        <Button onClick={handleNextPage}>Next Page</Button>
       </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
-        <label htmlFor="pageSize">Items per page:</label>
-        <select id="pageSize" value={size} onChange={handleSizeChange}>
-          <option value={5}>5</option>
-          <option value={10}>10</option>
-          <option value={20}>20</option>
-          <option value={50}>50</option>
-        </select>
-      </Box>
+      <FormControl>
+        <InputLabel>Items</InputLabel>
+        <Select id="pageSize" value={size.toString()} label="Items"onChange={handleSizeChange} autoWidth>
+          <MenuItem value={5}>5</MenuItem >
+          <MenuItem value={10}>10</MenuItem >
+          <MenuItem value={20}>20</MenuItem >
+          <MenuItem value={50}>50</MenuItem >
+        </Select>
+        </FormControl>
+      </Stack>
     </>
   );
 }
