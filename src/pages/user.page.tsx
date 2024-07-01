38b0dc13bc94f6815632fb038b0b67b195fda089
Finnick223 <filipf192@gmail.com>
2024-07-01 23:09:50 +0200

    add body figure and fields with measurements

--- src/pages/user.page.tsx
+++ src/pages/user.page.tsx
@@ -1,47 +1,88 @@
-import { useEffect, useState } from 'react';
-import { User, Configuration, UserApi } from '../client/src/index.ts';
-import { Box, CssBaseline, ListItemText } from '@mui/material';
-import BasicList from '../components/SimpleList.component.tsx';
-import { redirect } from 'react-router-dom';
+import { Button, CssBaseline, Grid, TextField } from '@mui/material';
+import { useState } from 'react';
+import { Form } from 'react-hook-form';
+import { BodyComponent } from 'reactjs-human-body';
+
 
 function UserPage() {
-  const [users, setUsers] = useState<User[]>([]);
+  const [params, setParams] = useState<any>();
 
-  const config = new Configuration({
-    username: 'admin',
-    password: 'admin',
-  });
-
-  useEffect(() => {
-    const api = new UserApi(config);
-
-    const loadUsers = async () => {
-      try {
-        const response = await api.getUsers();
-        const fetchedUsers = response;
-        setUsers(fetchedUsers);
-      } catch (error) {
-        console.error('Error fetching users: ', error);
-        redirect('/error');
-      }
-    };
-
-    loadUsers();
-  }, []);
+  const exampleParams = {
+    head: { selected: true },
+    left_arm: { show: false }
+  }
 
   return (
-    <Box>
+      <Grid container sx={{ flex: 1, px: 8 }}>
       <CssBaseline />
-      <BasicList
-        items={users}
-        renderItem={(user) => (
-          <ListItemText
-            primary={`${user.firstName} ${user.lastName}`}
-            secondary={user.email}
+        <Grid container xs={12} justifyContent="center" direction={'row'} >
+        <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Age"
+              variant="outlined"
+              margin="normal"
           />
-        )}
-      />
-    </Box>
+          <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Height"
+              variant="outlined"
+              margin="normal"
+          />
+          <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Weight"
+              variant="outlined"
+              margin="normal"
+          />
+        </Grid>
+        <Grid container xs={6} justifyContent="center" direction={'column'} > 
+          <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Arms"
+              variant="outlined"
+              margin="normal"
+          />
+          <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Chest"
+              variant="outlined"
+              margin="normal"
+              onClick={() => setParams({
+                chest: { selected: true },
+                })}
+          />
+          <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Belly"
+              variant="outlined"
+              margin="normal"
+              onClick={() => setParams(exampleParams)}
+              onSelect={() => setParams(exampleParams)}
+              onFocus={() => setParams(exampleParams)}
+          />
+          <TextField
+              // {...register("firstName")}
+              // defaultValue={user?.firstName}
+              label="Legs"
+              variant="outlined"
+              margin="normal"
+          />
+          <Button variant='contained' type='submit' sx={{mt: 4}}>
+            Update
+          </Button>
+        </Grid>
+        <Grid container xs={6} justifyContent="center"> 
+        <BodyComponent 
+          partsInput={params}
+        />
+        </Grid>
+      </Grid>
   );
 }
 
