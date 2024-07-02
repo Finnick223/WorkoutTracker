db0cf1e31ae35b68e75553116e87ce18595cdd85
Finnick223 <filipf192@gmail.com>
2024-07-02 13:22:48 +0200

    track bodypart on model when clicking input

--- src/pages/user.page.tsx
+++ src/pages/user.page.tsx
@@ -1,88 +1,94 @@
 import { Button, CssBaseline, Grid, TextField } from '@mui/material';
 import { useState } from 'react';
-import { Form } from 'react-hook-form';
 import { BodyComponent } from 'reactjs-human-body';
 
 
 function UserPage() {
   const [params, setParams] = useState<any>();
 
-  const exampleParams = {
-    head: { selected: true },
-    left_arm: { show: false }
-  }
-
+  const handlePartChange = (part: string) => {
+    setParams(null);
+    setTimeout(() => {
+      if(part==='arms'){
+        setParams({
+          leftArm: { selected: true },
+          rightArm: { selected: true },
+        })
+      }
+      else if(part==='legs'){
+        setParams({
+          leftLeg: {selected: true},
+          rightLeg: {selected: true},
+        })
+      } else{
+        setParams({
+        [part]: { selected: true },
+        })
+      }
+  }, 0);
+  };
+  
   return (
-      <Grid container sx={{ flex: 1, px: 8 }}>
+    <Grid container sx={{ flex: 1, px: 8 }}>
       <CssBaseline />
-        <Grid container xs={12} justifyContent="center" direction={'row'} >
+      <Grid container xs={12} justifyContent="center" direction={'row'}>
         <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Age"
-              variant="outlined"
-              margin="normal"
-          />
-          <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Height"
-              variant="outlined"
-              margin="normal"
-          />
-          <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Weight"
-              variant="outlined"
-              margin="normal"
-          />
-        </Grid>
-        <Grid container xs={6} justifyContent="center" direction={'column'} > 
-          <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Arms"
-              variant="outlined"
-              margin="normal"
-          />
-          <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Chest"
-              variant="outlined"
-              margin="normal"
-              onClick={() => setParams({
-                chest: { selected: true },
-                })}
-          />
-          <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Belly"
-              variant="outlined"
-              margin="normal"
-              onClick={() => setParams(exampleParams)}
-              onSelect={() => setParams(exampleParams)}
-              onFocus={() => setParams(exampleParams)}
-          />
-          <TextField
-              // {...register("firstName")}
-              // defaultValue={user?.firstName}
-              label="Legs"
-              variant="outlined"
-              margin="normal"
-          />
-          <Button variant='contained' type='submit' sx={{mt: 4}}>
-            Update
-          </Button>
-        </Grid>
-        <Grid container xs={6} justifyContent="center"> 
-        <BodyComponent 
-          partsInput={params}
+          label="Age"
+          variant="outlined"
+          margin="normal"
         />
-        </Grid>
+        <TextField
+          label="Height"
+          variant="outlined"
+          margin="normal"
+        />
+        <TextField
+          label="Weight"
+          variant="outlined"
+          margin="normal"
+        />
       </Grid>
+      <Grid container xs={6} justifyContent="center" direction={'column'}>
+        <TextField
+          label="Arms"
+          variant="outlined"
+          margin="normal"
+          onClick={() => handlePartChange('arms')}
+        />
+        <TextField
+          label="Chest"
+          variant="outlined"
+          margin="normal"
+          onClick={() => handlePartChange('chest')}
+        />
+        <TextField
+          label="Stomach"
+          variant="outlined"
+          margin="normal"
+          onClick={() => handlePartChange('stomach')}
+        />
+        <TextField
+          label="Legs"
+          variant="outlined"
+          margin="normal"
+          onClick={() => handlePartChange('legs')}
+        />
+        <Button variant='contained' type='submit' sx={{ mt: 4 }}>
+          Update
+        </Button>
+      </Grid>
+      <Grid container xs={6} justifyContent="center">
+        {params ? (
+            <BodyComponent
+              partsInput={params}
+            />
+        ) : (
+          <div>
+            <BodyComponent />
+          </div>
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
