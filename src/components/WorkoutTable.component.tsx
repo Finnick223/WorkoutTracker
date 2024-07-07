ede2483de18b6e655ab282b7aa916587314d6635
Finnick223 <filipf192@gmail.com>
2024-07-07 12:47:05 +0200

    add prototype datagrid

--- src/components/WorkoutTable.component.tsx
+++ src/components/WorkoutTable.component.tsx
@@ -1,48 +1,103 @@
-export default function Table() {
+import { Box, Button } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import { DataGrid, GridColDef, GridRowModes, GridRowModesModel, GridRowsProp, GridSlots, GridToolbarContainer } from "@mui/x-data-grid";
+import { useState } from "react";
+
+
+
+
+const initialRows = [
+  {
+    id: Math.random().toString(36).substr(2, 9),
+    name: 'ława',
+    set1: '12',
+    set2: '10'
+  },
+  {
+    id: Math.random().toString(36).substr(2, 9),
+    name: 'siady',
+    set1: '10',
+    set2: '8'
+  },
+];
+
+interface EditToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
+  ) => void;
+}
+function EditToolbar(props: EditToolbarProps) {
+  const {setRows, setRowModesModel} = props;
+
+  const handleClick = () => {
+    const id = Math.random().toString(36).substr(2, 9);
+    setRows((oldRows) => [...oldRows, { id, name: '', set1: '', set2: ''}]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+    }));
+  };
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th rowSpan={2}>Exercise</th>
-          <th colSpan={2}>Set1</th>
-          <th colSpan={2}>Set2</th>
-          <th colSpan={2}>Set3</th>
-        </tr>
-        <tr>
-          <th>weight</th>
-          <th>reps</th>
-          <th>weight</th>
-          <th>reps</th>
-          <th>weight</th>
-          <th>reps</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>ława</td>
-          <td>50</td>
-          <td>5</td>
-          <td>55</td>
-          <td>5</td>
-          <td>50</td>
-          <td>5</td>
-        </tr>
-        <tr>
-          <td>siady</td>
-          <td>100</td>
-          <td>10</td>
-          <td>150</td>
-          <td>5</td>
-          <td>150</td>
-          <td>4</td>
-        </tr>
-      </tbody>
-      {/* <tfoot>
-        <tr>
-          <td>Sum</td>
-          <td>$180</td>
-        </tr>
-      </tfoot> */}
-    </table>
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add record
+      </Button>
+    </GridToolbarContainer>
+  );
+}
+
+export default function Table() {
+  const [rows, setRows] = useState(initialRows);
+  const [rowModesModel, setRowModesModel] = useState({});
+
+  const columns: GridColDef[] = [
+    {field: 'name', headerName: 'Exercise', width: 180, editable: true},
+    {
+      field: 'set1',
+      headerName: 'set1',
+      type: 'number',
+      width: 80,
+      align: 'left',
+      headerAlign: 'left',
+      editable: true,
+    },
+    {
+      field: 'set2',
+      headerName: 'set2',
+      type: 'number',
+      width: 80,
+      align: 'left',
+      headerAlign: 'left',
+      editable: true,
+    },
+  ]
+  return (
+    <Box
+      sx={{
+        height: 500,
+        width: '100%',
+        '& .actions': {
+          color: 'text.secondary',
+        },
+        '& .textPrimary': {
+          color: 'text.primary',
+        },
+      }}
+    >
+      <DataGrid 
+        rows={rows}
+        columns={columns}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        slots={{
+          toolbar: EditToolbar as GridSlots['toolbar'],
+        }}
+        slotProps={{
+          toolbar: { setRows, setRowModesModel },
+        }}
+      />
+
+    </Box>
   );
 }
--- src/pages/Training/trainingDetail.page.tsx
+++ src/pages/Training/trainingDetail.page.tsx
@@ -1,10 +1,12 @@
 // import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import Table from '../../components/WorkoutTable.component';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <section>
         <Link to=".." relative="path" className="back-button">
           &larr; <span>Back to all trainings</span>
