174b25f6d53a120605cd65e7890e2796da076f1b
Finnick223 <filipf192@gmail.com>
2024-07-12 15:57:16 +0200

    Setup environment for tests

---
+++ src/utils/tests/setupTests.tsx
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom'
+import * as matchers from '@testing-library/jest-dom/matchers'
+import { afterAll, afterEach, beforeAll, expect, vitest } from 'vitest'
+import { server } from './mocks/server'
+
+expect.extend(matchers)
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vitest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addEventListener: vitest.fn(),
+        dispatchEvent: vitest.fn()
+    }))
+})
+
+Object.defineProperty(window, 'IntersectionObserver', {
+    writable: true,
+    value: vitest.fn().mockReturnValue({
+      observe: vitest.fn(),
+      unobserve: vitest.fn(),
+      disconnect: vitest.fn()
+    })
+  })
+
+  server.events.on('request:start', ({ request }) => {
+    console.log('Outgoing:', request.method, request.url)
+  })  
+
+beforeAll(() => server.listen()) // Enable the mocking in tests.
+afterEach(() => server.resetHandlers()) // Reset any runtime handlers tests may use.
+afterAll(() => server.close()) // Clean up once the tests are done.
