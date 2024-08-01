4dfac0c5e1f74bf403a9ce0216e6a396fc892f77
Finnick223 <filipf192@gmail.com>
2024-08-01 19:20:26 +0200

    add switching theme modes and store it in localstorage

--- src/providers/MaterialUI.provider.tsx
+++ src/providers/MaterialUI.provider.tsx
@@ -1,13 +1,43 @@
 import { createTheme, PaletteMode, ThemeProvider } from '@mui/material';
-import { PropsWithChildren, useMemo, useState } from 'react';
+import {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import { getDesignThemes } from 'src/styles/theme';
+
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 export const MaterialProvider = ({
   children,
 }: PropsWithChildren<Record<string, unknown>>) => {
-  const [mode] = useState<PaletteMode>('light');
+  const [mode, setMode] = useState<PaletteMode>('light');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedMode = window.localStorage.getItem('theme');
+      if (storedMode === 'light' || storedMode === 'dark') setMode(storedMode);
+    }
+  }, []);
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        const newMode = mode === 'light' ? 'dark' : 'light';
+        localStorage.setItem('theme', newMode);
+        setMode(newMode);
+      },
+    }),
+    [mode],
+  );
 
   const theme = useMemo(() => createTheme(getDesignThemes(mode)), [mode]);
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+    </ColorModeContext.Provider>
+  );
 };
