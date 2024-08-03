e3051bcc8331a52f10a7fc7b28913f99d47de69e
Finnick223 <filipf192@gmail.com>
2024-08-03 13:18:23 +0200

    style footer

--- src/modules/Footer/Footer.component.tsx
+++ src/modules/Footer/Footer.component.tsx
@@ -1,49 +1,122 @@
-import { Stack, Typography } from '@mui/material';
+import { Link, Stack, Typography } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 export const Footer = () => {
   return (
     <Grid2
       container
-      py={2}
+      py={3}
+      direction="column"
+      rowSpacing={3}
       sx={{
-        backgroundColor: 'gray',
+        bgcolor: '#3b3b3b',
       }}
     >
-      <Grid2 md={4}>
+      <Grid2>
         <Stack
-          direction="column"
+          direction="row"
           justifyContent="center"
           alignItems="center"
-          spacing={2}
+          spacing={3}
         >
-          <Typography>dupa</Typography>
-          <Typography>dupa</Typography>
-          <Typography>dupa</Typography>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            About
+          </Link>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            Team
+          </Link>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            Privacy Policy
+          </Link>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            Terms
+          </Link>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            Contact
+          </Link>
         </Stack>
       </Grid2>
-      <Grid2 md={4}>
+      <Grid2>
         <Stack
-          direction="column"
+          direction="row"
           justifyContent="center"
           alignItems="center"
           spacing={2}
         >
-          <Typography>dupa</Typography>
-          <Typography>dupa</Typography>
-          <Typography>dupa</Typography>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            <Stack alignItems="center">
+              <span>GitHub</span>
+              <svg
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                height="1.5rem"
+                width="1.5rem"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </Stack>
+          </Link>
+          <Link
+            href="#"
+            underline="none"
+            color="white"
+            sx={{ ':hover': { color: 'gray' } }}
+          >
+            <Stack alignItems="center">
+              <span>LinkedIn</span>
+              <svg
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                height="1.5rem"
+                width="1.5rem"
+              >
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
+            </Stack>
+          </Link>
         </Stack>
       </Grid2>
-      <Grid2 md={4}>
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={2}
-        >
-          <Typography>dupa</Typography>
-          <Typography>dupa</Typography>
-          <Typography>dupa</Typography>
+      <Grid2>
+        <Stack justifyContent="center" alignItems="center">
+          <Typography color="white">
+            © 2024 Owner. All rights reserved.
+          </Typography>
         </Stack>
       </Grid2>
     </Grid2>
