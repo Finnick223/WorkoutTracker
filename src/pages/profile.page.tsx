0dd854c83010968be0bc3c5a4bc83646a93a70e7
Finnick223 <filipf192@gmail.com>
2024-08-30 19:26:35 +0200

    Add prototype profile page

--- src/pages/profile.page.tsx
+++ src/pages/profile.page.tsx
@@ -5,6 +5,9 @@ import {
   Avatar,
   CircularProgress,
   Button,
+  Paper,
+  Stack,
+  IconButton,
 } from '@mui/material';
 import useAuthStatus from 'src/hooks/useAuth.ts';
 import { getCurrentUser } from 'src/api/auth';
@@ -12,6 +15,7 @@ import { User } from 'src/client/src';
 import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import EditNoteIcon from '@mui/icons-material/EditNote';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import EditUserModal from 'src/components/modals/EditProfile.modal.tsx';
 import { useModal } from 'src/components/modals/Error.modal';
 import { AnimatePage } from 'src/animations/AnimatePage';
@@ -60,37 +64,169 @@ function Profile() {
   return (
     <>
       <AnimatePage>
-        <Container maxWidth="lg">
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+        <Container maxWidth="sm">
+          <Stack
+            direction={'column'}
+            justifyContent={'space-around'}
+            alignItems={'center'}
+            spacing={2}
+            sx={{ mb: 8 }}
           >
-            <Avatar sx={{ m: 1 }}></Avatar>
-            <Typography variant="h2" sx={{ mb: 4 }}>
-              {user?.firstName}'s Profile
-            </Typography>
-            <Box sx={{ alignItems: 'flex-start' }}>
-              <Typography variant="h6">Email: {user?.email}</Typography>
-              <Typography variant="h6">
-                First Name: {user?.firstName}
+            <Typography variant="h3">{user?.firstName}'s Profile</Typography>
+            <Paper
+              component={Box}
+              width={'100%'}
+              sx={{ m: 2, p: 2, borderRadius: 2 }}
+            >
+              <Typography mb={1} variant="body2" color={'text.secondary'}>
+                Profile picture
               </Typography>
-              <Typography variant="h6">Last Name: {user?.lastName}</Typography>
-              <Typography variant="h6">Gender: {user?.genders}</Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<EditNoteIcon />}
-                fullWidth
-                onClick={handleEditOpen}
-                sx={{ my: 2 }}
+              <Stack direction={'row'} spacing={2}>
+                <Avatar />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<EditNoteIcon />}
+                  fullWidth
+                  // onClick={handleEditOpen}
+                  sx={{ my: 2 }}
+                >
+                  Change
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  startIcon={<HighlightOffIcon />}
+                  fullWidth
+                  // onClick={handleEditOpen}
+                  sx={{ my: 2 }}
+                >
+                  Remove
+                </Button>
+              </Stack>
+            </Paper>
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              spacing={2}
+              width={'100%'}
+            >
+              <Paper
+                sx={{
+                  p: 4,
+                  borderRadius: 2,
+                }}
               >
-                Edit
-              </Button>
-            </Box>
-          </Box>
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography
+                    variant="h6"
+                    sx={{ color: 'primary.main', fontWeight: 'bold' }}
+                  >
+                    Personal Information
+                  </Typography>
+                  <IconButton onClick={handleEditOpen} color="primary">
+                    <EditNoteIcon />
+                  </IconButton>
+                </Box>
+                <Stack spacing={2} mt={2}>
+                  <Box>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: 'text.secondary' }}
+                    >
+                      First Name
+                    </Typography>
+                    <Typography variant="h6">{user?.firstName}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: 'text.secondary' }}
+                    >
+                      Last Name
+                    </Typography>
+                    <Typography variant="h6">{user?.lastName}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: 'text.secondary' }}
+                    >
+                      Email address
+                    </Typography>
+                    <Typography variant="h6">{user?.email}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography variant="body2" color={'text.secondary'}>
+                      Gender
+                    </Typography>
+                    <Typography variant="h6">{user?.genders}</Typography>
+                  </Box>
+                </Stack>
+              </Paper>
+              {/* 2 column */}
+              <Paper
+                sx={{
+                  p: 4,
+                  borderRadius: 2,
+                }}
+              >
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography
+                    variant="h6"
+                    sx={{ color: 'primary.main', fontWeight: 'bold' }}
+                  >
+                    User Settings
+                  </Typography>
+                  <IconButton onClick={handleEditOpen} color="primary">
+                    <EditNoteIcon />
+                  </IconButton>
+                </Box>
+                <Stack spacing={2} mt={2}>
+                  <Box>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: 'text.secondary' }}
+                    >
+                      First Name
+                    </Typography>
+                    <Typography variant="h6">{user?.firstName}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: 'text.secondary' }}
+                    >
+                      Last Name
+                    </Typography>
+                    <Typography variant="h6">{user?.lastName}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: 'text.secondary' }}
+                    >
+                      Email address
+                    </Typography>
+                    <Typography variant="h6">{user?.email}</Typography>
+                  </Box>
+                  <Box>
+                    <Typography variant="body2" color={'text.secondary'}>
+                      Gender
+                    </Typography>
+                    <Typography variant="h6">{user?.genders}</Typography>
+                  </Box>
+                </Stack>
+              </Paper>
+            </Stack>
+          </Stack>
         </Container>
       </AnimatePage>
       <EditUserModal
