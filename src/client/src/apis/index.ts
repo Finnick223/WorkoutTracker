96a4f3904f304160dfff70a5651ccab83e05bd60
Finnick223 <filipf192@gmail.com>
2024-09-13 15:25:17 +0200

    Update api

--- src/client/src/apis/index.ts
+++ src/client/src/apis/index.ts
@@ -1,3 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ExerciseApi';
 export * from './TrainingApi';
 export * from './UserApi';
 export * from './UserMeasurementApi';
--- src/client/src/models/index.ts
+++ src/client/src/models/index.ts
@@ -1,3 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
 export * from './ErrorResponse';
 export * from './Exercise';
 export * from './ExerciseCreate';
