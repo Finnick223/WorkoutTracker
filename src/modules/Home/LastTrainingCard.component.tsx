cf39d641bf5252c78fbb46e6465c03d76fcfb03b
Finnick223 <filipf192@gmail.com>
2024-10-21 21:59:03 +0200

    Add defensive check and conditional rendering to prevent errors for new users

--- src/modules/Home/LastTrainingCard.component.tsx
+++ src/modules/Home/LastTrainingCard.component.tsx
@@ -5,18 +5,33 @@ import { LastTrainingCardProps } from 'src/interfaces/training.interfaces';
 
 export const LastTrainingCard: React.FC<LastTrainingCardProps> = ({
   trainingData,
-}) => (
-  <Paper component={Box} p={2} sx={{ height: '100%' }}>
-    <Typography gutterBottom color={'text.secondary'}>
-      Last training at {trainingData[0].createdOn?.slice(0, 10)}
-    </Typography>
-    <Box display="flex" alignItems="center" mt={{ xs: 4, sm: 2, md: 4 }}>
-      <CustomLink href={`/training/${trainingData[0].id}`} color="inherit">
-        <Button>
-          {trainingData[0].name} {trainingData[0].description}{' '}
-          <ArrowForwardIcon />
-        </Button>
-      </CustomLink>
-    </Box>
-  </Paper>
-);
+}) => {
+  const hasTrainingData =
+    Array.isArray(trainingData) && trainingData.length > 0;
+  return (
+    <Paper component={Box} p={2} sx={{ height: '100%' }}>
+      {hasTrainingData ? (
+        <>
+          <Typography gutterBottom color={'text.secondary'}>
+            Last training at {trainingData[0].createdOn?.slice(0, 10)}
+          </Typography>
+          <Box display="flex" alignItems="center" mt={{ xs: 4, sm: 2, md: 4 }}>
+            <CustomLink
+              href={`/training/${trainingData[0].id}`}
+              color="inherit"
+            >
+              <Button>
+                {trainingData[0].name} {trainingData[0].description}{' '}
+                <ArrowForwardIcon />
+              </Button>
+            </CustomLink>
+          </Box>
+        </>
+      ) : (
+        <Typography gutterBottom color="text.secondary">
+          No trainings available
+        </Typography>
+      )}
+    </Paper>
+  );
+};
